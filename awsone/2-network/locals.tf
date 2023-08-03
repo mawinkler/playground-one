a84228657f3efbab5d83ba07fc5796bea737b478
Markus Winkler <winkler.info@icloud.com>
2023-08-03 12:29:00 +0000

    support for access_ip lists

--- awsone/2-network/locals.tf
+++ awsone/2-network/locals.tf
@@ -14,38 +14,38 @@ locals {
           from        = 22
           to          = 22
           protocol    = "tcp"
-          cidr_blocks = [var.access_ip]
+          cidr_blocks = var.access_ip
         }
         http = {
           from        = 80
           to          = 80
           protocol    = "tcp"
-          cidr_blocks = [var.access_ip]
+          cidr_blocks = var.access_ip
         }
         https = {
           from        = 443
           to          = 443
           protocol    = "tcp"
-          cidr_blocks = [var.access_ip]
+          cidr_blocks = var.access_ip
         }
         rdp = {
           from        = 3389
           to          = 3389
           protocol    = "tcp"
-          cidr_blocks = [var.access_ip]
+          cidr_blocks = var.access_ip
         }
         winrm = {
           description = "WinRM Access"
           from        = 5985
           to          = 5986
           protocol    = "tcp"
-          cidr_blocks = [var.access_ip]
+          cidr_blocks = var.access_ip
         }
         http = {
           from        = 8080
           to          = 8080
           protocol    = "tcp"
-          cidr_blocks = [var.access_ip]
+          cidr_blocks = var.access_ip
         }
       }
     }
--- templates/terraform-2-network.tfvars
+++ templates/terraform-2-network.tfvars
@@ -7,7 +7,7 @@ aws_region = "${AWS_REGION}"
 # Allow access to the environment from any location or restrict it to your public ip
 # Example:
 #   access_ip      = "<YOUR IP>/32"
-access_ip = "${ACCESS_IP}"
+access_ip = ${ACCESS_IP}
 
 # Environment Name
 environment = "${AWS_ENVIRONMENT}"
