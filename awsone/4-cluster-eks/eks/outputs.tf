d62070298fbd6479d6141be38ca2c2bab5f21e2c
Markus Winkler <winkler.info@icloud.com>
2023-09-15 08:37:56 +0000

    added cluster arn

--- awsone/4-cluster-eks/eks/outputs.tf
+++ awsone/4-cluster-eks/eks/outputs.tf
@@ -5,3 +5,8 @@ output "update_local_context_command" {
   description = "Command to update local kube context"
   value       = "aws eks update-kubeconfig --name=${module.eks.cluster_name} --alias=${module.eks.cluster_name} --region=${var.aws_region}"
 }
+
+output "cluster_arn" {
+  description = "Cluster ARN"
+  value       = data.aws_eks_cluster.eks.arn
+}
--- awsone/4-cluster-eks/outputs.tf
+++ awsone/4-cluster-eks/outputs.tf
@@ -5,3 +5,8 @@ output "update_local_context_command" {
   description = "Update local Cluster Context command"
   value       = module.eks.update_local_context_command
 }
+
+output "cluster_arn" {
+  description = "Cluster ARN"
+  value       = module.eks.cluster_arn
+}
