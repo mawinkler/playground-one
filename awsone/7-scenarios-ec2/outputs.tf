2576196cd58c8d713bce8d4571dc074d2cc03a97
Markus Winkler <winkler.info@icloud.com>
2024-02-13 12:33:23 +0100

    openssl3 demo app removed

--- awsone/7-scenarios-ec2/outputs.tf
+++ awsone/7-scenarios-ec2/outputs.tf
@@ -6,11 +6,6 @@ output "loadbalancer_dns_java_goof" {
   value = module.victims.loadbalancer_dns_java_goof
 }
 
-output "loadbalancer_dns_openssl3" {
-  description = "Loadbalancer OpenSSL DNS name"
-  value = module.victims.loadbalancer_dns_openssl3
-}
-
 output "loadbalancer_dns_system_monitor" {
   description = "Loadbalancer System Monitor DNS name"
   value       = module.goat.loadbalancer_dns_system_monitor
--- awsone/7-scenarios-ec2/victims/outputs.tf
+++ awsone/7-scenarios-ec2/victims/outputs.tf
@@ -1,7 +1,3 @@
 output "loadbalancer_dns_java_goof" {
   value = kubernetes_ingress_v1.java_goof_ingress.status[0].load_balancer[0].ingress[0].hostname
 }
-
-output "loadbalancer_dns_openssl3" {
-  value = kubernetes_ingress_v1.openssl3_ingress.status[0].load_balancer[0].ingress[0].hostname
-}
