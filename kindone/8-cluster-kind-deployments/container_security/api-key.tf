7bfeb55b9edc253345e3e22d6951703aebe0a17c
Markus Winkler <winkler.info@icloud.com>
2024-02-19 09:40:37 +0100

    proper life-cycle delete

--- kindone/8-cluster-kind-deployments/container_security/api-key.tf
+++ kindone/8-cluster-kind-deployments/container_security/api-key.tf
@@ -1,22 +1,7 @@
 # ####################################
 # Container Security Cluster API
 # ####################################
-resource "null_resource" "always_run" {
-  triggers = {
-    timestamp = "${timestamp()}"
-  }
-}
-
-resource "random_string" "suffix" {
-  length  = 8
-  lower   = true
-  upper   = false
-  numeric = true
-  special = false
-}
-
 resource "restapi_object" "cluster" {
-
   provider       = restapi.container_security
   path           = "/beta/containerSecurity/kubernetesClusters"
   create_method  = "POST"
@@ -24,21 +9,9 @@ resource "restapi_object" "cluster" {
   id_attribute   = "name"
   data           = <<-EOT
     {
-      "name": "${replace(var.cluster_name, "-", "_")}",
+      "name": "${replace(var.environment, "-", "_")}",
       "description": "Playground Cluster",
       "policyID": "${local.cluster_policy}"
     }
   EOT
-
-  lifecycle {
-    replace_triggered_by = [
-      null_resource.always_run
-    ]
-  }
-}
-
-# Store Cluster API Key and endpoint for use in the helm installation step
-locals {
-  cluster_apikey   = jsondecode(restapi_object.cluster.api_response).apiKey
-  cluster_endpoint = jsondecode(restapi_object.cluster.api_response).endpointUrl
 }
--- kindone/8-cluster-kind-deployments/outputs.tf
+++ kindone/8-cluster-kind-deployments/outputs.tf
@@ -11,6 +11,15 @@ output "loadbalancer_dns_grafana" {
   value       = var.prometheus ? module.prometheus[0].loadbalancer_dns_grafana : null
 }
 
-output "policy_id" {
-  value = var.container_security ? module.container_security[0].policy_id : null
+output "cluster_id" {
+  value = var.container_security ? module.container_security[0].cluster_id : null
+}
+
+output "cluster_apikey" {
+  value     = var.container_security ? module.container_security[0].cluster_apikey : null
+  sensitive = true
+}
+
+output "cluster_policy_id" {
+  value = var.container_security ? module.container_security[0].cluster_policy_id : null
 }
