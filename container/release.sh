79ae1d132a54cc699a65b60d71bb119c971bb4c6
Markus Winkler <winkler.info@icloud.com>
2024-02-20 08:32:01 +0100

    life-cycle

--- container/release.sh
+++ container/release.sh
@@ -3,20 +3,18 @@ PGO_VERSION=$(cat .PGO_VERSION)
 
 printf '%s\n' "Building mcs version ${PGO_VERSION}"
 
-# docker buildx create \
-#   --name container \
-#   --driver=docker-container 
-#   container
-# docker run --privileged multiarch/qemu-user-static:latest --reset -p yes --credential yes
-
     # --progress=plain \
     # --no-cache \
-    # -t mawinkler/pgoc:latest \
 docker buildx build \
     -t mawinkler/pgoc:${PGO_VERSION} \
+    -t mawinkler/pgoc:latest \
     --build-arg uid=$(id -u) \
     --build-arg gid=$(id -g) \
     --build-arg version=${PGO_VERSION} \
     --platform linux/amd64,linux/arm64/v8 \
     --push \
     .
+
+cd ..
+git tag ${PGO_VERSION}
+git push --tags
