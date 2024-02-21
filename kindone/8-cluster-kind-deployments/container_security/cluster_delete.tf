c9b07d1bc1ccd0a140fdbf9f7e4a2754b0d2b289
Markus Winkler <winkler.info@icloud.com>
2024-02-21 17:39:37 +0100

    hot fix container security

--- kindone/8-cluster-kind-deployments/container_security/cluster_delete.tf
+++ kindone/8-cluster-kind-deployments/container_security/cluster_delete.tf
@@ -1,18 +1,18 @@
 # ####################################
 # Delete cluster from Vision One on destroy
 # ####################################
-resource "null_resource" "cluster_delete" {
-  triggers = {
-    api_key    = var.api_key
-    cluster_id = local.cluster_id
-  }
-  provisioner "local-exec" {
-    when    = destroy
-    command = <<SCRIPT
-      curl -fsSL -X DELETE \
-        -H "Content-Type: application/json" \
-        -H "api-version: v1" -H "Authorization: Bearer ${self.triggers.api_key}" \
-        "https://api.xdr.trendmicro.com/beta/containerSecurity/kubernetesClusters/${self.triggers.cluster_id}"
-    SCRIPT
-  }
-}
+# resource "null_resource" "cluster_delete" {
+#   triggers = {
+#     api_key    = var.api_key
+#     cluster_id = local.cluster_id
+#   }
+#   provisioner "local-exec" {
+#     when    = destroy
+#     command = <<SCRIPT
+#       curl -fsSL -X DELETE \
+#         -H "Content-Type: application/json" \
+#         -H "api-version: v1" -H "Authorization: Bearer ${self.triggers.api_key}" \
+#         "https://api.xdr.trendmicro.com/beta/containerSecurity/kubernetesClusters/${self.triggers.cluster_id}"
+#     SCRIPT
+#   }
+# }
