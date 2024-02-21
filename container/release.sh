d598a364ff6a866e8d684e5876713a72fa5efbe1
Markus Winkler <winkler.info@icloud.com>
2024-02-21 08:53:37 +0100

    no-cache for release

--- container/release.sh
+++ container/release.sh
@@ -4,10 +4,10 @@ PGO_VERSION=$(cat .PGO_VERSION)
 printf '%s\n' "Building mcs version ${PGO_VERSION}"
 
     # --progress=plain \
-    # --no-cache \
 docker buildx build \
     -t mawinkler/pgoc:${PGO_VERSION} \
     -t mawinkler/pgoc:latest \
+    --no-cache \
     --build-arg uid=$(id -u) \
     --build-arg gid=$(id -g) \
     --build-arg version=${PGO_VERSION} \
