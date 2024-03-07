49eef7e4040e980da1ae248ff03d93f803f1173a
Markus Winkler <winkler.info@icloud.com>
2024-03-07 17:31:14 +0100

    kubectl

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -647,10 +647,13 @@ function ensure_kubectl() {
 function ensure_kubectl_apt() {
 
   if ! command -v kubectl &>/dev/null; then
-    curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - && \
-      echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list && \
-      sudo apt-get update && \
-      sudo apt-get install -y kubectl
+    sudo mkdir -p -m 755 /etc/apt/keyrings
+    curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+    echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+    # curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - && \
+    #   echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list && \
+    sudo apt-get update &&
+    sudo apt-get install -y kubectl
   else
     sudo apt-get upgrade -y kubectl
   fi
