c74d46f86efdaf2b92b1bfa125f72574a1a24c4d
Markus Winkler <winkler.info@icloud.com>
2024-03-19 10:16:02 +0100

    changed back no of subnets

--- awsone/9-deep-security/vpc/locals.tf
+++ awsone/9-deep-security/vpc/locals.tf
@@ -5,5 +5,5 @@ locals {
   name = "${var.environment}-vpc"
 
   vpc_cidr = "10.0.0.0/16"
-  azs      = slice(data.aws_availability_zones.available.names, 0, 1)
+  azs      = slice(data.aws_availability_zones.available.names, 0, 2)
 }
