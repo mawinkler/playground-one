3477d804640dce1e051ca71e785bfe37a847dfac
Markus Winkler <winkler.info@icloud.com>
2024-10-24 11:42:40 +0200

    improve compliance

--- awsone/6-bucket-scanner/scanner/s3.tf
+++ awsone/6-bucket-scanner/scanner/s3.tf
@@ -20,6 +20,59 @@ resource "aws_s3_bucket_ownership_controls" "playground_awsone" {
   }
 }
 
+resource "aws_s3_bucket_accelerate_configuration" "playground_awsone" {
+  bucket = aws_s3_bucket.playground_awsone.id
+  status = "Enabled"
+}
+
+resource "aws_s3_bucket_versioning" "playground_awsone" {
+  bucket = aws_s3_bucket.playground_awsone.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_object_lock_configuration" "playground_awsone" {
+  bucket = aws_s3_bucket.playground_awsone.id
+  rule {
+    default_retention {
+      mode = "COMPLIANCE"
+      days = 5
+    }
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "playground_awsone" {
+  bucket = aws_s3_bucket.playground_awsone.id
+
+  rule {
+    id = "transition-malicious-files"
+
+    filter {
+      tag {
+        key   = "fss-scan-result"
+        value = "malicious"
+      }
+    }
+
+    transition {
+      days          = 30
+      storage_class = "STANDARD_IA"
+    }
+
+    transition {
+      days          = 60
+      storage_class = "GLACIER"
+    }
+
+    expiration {
+      days = 90
+    }
+
+    status = "Enabled"
+  }
+}
+
 resource "aws_s3_bucket_notification" "bucket_notification" {
   bucket = aws_s3_bucket.playground_awsone.id
 
