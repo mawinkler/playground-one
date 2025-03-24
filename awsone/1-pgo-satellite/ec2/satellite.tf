b352f94121c522f3f64638593cba4687cee28fa2
Markus Winkler <winkler.info@icloud.com>
2025-03-24 17:05:20 +0100

    resize to 40GB

--- awsone/1-pgo-satellite/ec2/satellite.tf
+++ awsone/1-pgo-satellite/ec2/satellite.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "pgo-satellite" {
   key_name               = aws_key_pair.key_pair.key_name
 
   root_block_device {
-    volume_size           = 30
+    volume_size           = 40
   }
 
   tags = {
